129a5709bd6dd07eaf81cecb1f8819c65e6233bc
Fabián Rodriguez Rodriguez <fabianrguezrguez@gmail.com>
2018-06-15 16:59:59 +0100

    Se arregla el menú lateral

--- src/tsx/components/Login.tsx
+++ src/tsx/components/Login.tsx
@@ -3,20 +3,41 @@ import * as styles from '../../css/login.css';
 import { inject, observer } from 'mobx-react';
 import { LoginStore } from '../stores/LoginStore';
 
-export interface HeaderProps {
+export interface LoginProps {
 	loginStore?: LoginStore;
+}
+
+export interface LoginState {
+	name: string;
+	password: string;
 }
 
 @inject('loginStore')
 @observer
-export default class Login extends React.Component<HeaderProps, {}> {
+export default class Login extends React.Component<LoginProps, LoginState> {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			name: '',
+			password: ''
+		};
+
 		this.toggleLogin = this.toggleLogin.bind(this);
+		this.catchUser = this.catchUser.bind(this);
 	}
 
 	toggleLogin() {
 		this.props.loginStore.toggleLogin();
+	}
+
+	catchUser(event) {
+		this.setState({ [event.target.name]: event.target.value });
+	}
+
+	sendLogin(event) {
+		event.preventDefault();
+		alert(`Iniciando sesión con: ${JSON.stringify(this.state.name)}`);
 	}
 
 	checkLogin() {
@@ -29,8 +50,8 @@ export default class Login extends React.Component<HeaderProps, {}> {
 						<div className={styles.login}>
 							<h1>Bienvenido a Where Is Your Car</h1>
 							<form>
-								<p><label>Usuario: </label><input /></p>
-								<p><label>Contraseña: </label><input type='password' /></p>
+								<p><label>Usuario: </label><input name='user' value={this.state.name} onChange={this.catchUser} /></p>
+								<p><label>Contraseña: </label><input name='password' type='password' value={this.state.password} onChange={this.catchUser} /></p>
 								<button type='submit' onClick={this.checkLogin}>Iniciar</button>
 							</form>
 						</div>
--- src/tsx/components/michal/drawer/SwipeableTemporaryDrawer.tsx
+++ src/tsx/components/michal/drawer/SwipeableTemporaryDrawer.tsx
@@ -10,106 +10,106 @@ import MapContainer from '../map/MapContainer';
 
 
 const styles = {
-  drawer: {
-    background: '#80d7ff',
-    height: '100%'
-  },
-  UserIcon: {
-    width: 175,
-    height: 175,
-    margin: 'auto',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'black'
-  },
-  list: {
-    width: 250,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 'auto'
-  },
-  info: {
-    display: 'flex',
-    margin: 'auto',
-    height: '51%',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end'
-  }
+	drawer: {
+		background: '#80d7ff',
+		height: '100%'
+	},
+	UserIcon: {
+		width: 175,
+		height: 175,
+		margin: 'auto',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		color: 'black'
+	},
+	list: {
+		width: 250,
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 'auto'
+	},
+	info: {
+		display: 'flex',
+		margin: 'auto',
+		height: '51%',
+		justifyContent: 'flex-end',
+		alignItems: 'flex-end'
+	}
 };
 
 export interface SwipeableTemporaryDrawerState {
-  open: boolean;
+	open: boolean;
 }
 
 export interface SwipeableTemporaryDrawerProps {
-  classes: any;
+	classes: any;
 }
 
 class SwipeableTemporaryDrawer extends React.Component<SwipeableTemporaryDrawerProps, SwipeableTemporaryDrawerState> {
-  constructor(props: SwipeableTemporaryDrawerProps) {
-    super(props);
-    this.state = {
-      open: false
-    };
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-  }
+	constructor(props: SwipeableTemporaryDrawerProps) {
+		super(props);
+		this.state = {
+			open: false
+		};
+		this.toggleDrawer = this.toggleDrawer.bind(this);
+	}
 
-  toggleDrawer() {
-    this.setState({ open: !this.state.open });
-  }
+	toggleDrawer() {
+		this.setState({ open: !this.state.open });
+	}
 
-  onChange() {
+	onChange() {
 
-  }
+	}
 
 
-  render() {
-    const { classes } = this.props;
-    return <div>
-      <div className={SwipeableDrawerStyle.topStuff}>
-        <div className={SwipeableDrawerStyle.menuButton}>
-          <Button onClick={this.toggleDrawer}><FontAwesome.FaBars size={30} /></Button>
-        </div>
-        <div className={SwipeableDrawerStyle.topButton}>
-          <button onClick={this.onChange} className={SwipeableDrawerStyle.funcButton}><span>Park</span></button>
-          <button className={SwipeableDrawerStyle.funcButton}><span>Move</span></button>
-          <button className={SwipeableDrawerStyle.funcButton}><span>Where did I park?</span></button>
-          <button className={SwipeableDrawerStyle.funcButton} style={{fontSize: '14px'}}><span>Where should I park?<FontAwesome.FaStar/></span></button>
-        </div>
-      </div>
-      <SwipeableDrawer open={this.state.open} onClose={this.toggleDrawer} onOpen={this.toggleDrawer}>
-        <div className={classes.drawer} tabIndex={0} role='button' onClick={this.toggleDrawer} onKeyDown={this.toggleDrawer}>
-          <div className={classes.UserIcon}><FontAwesome.FaUser size={40} /></div>
-          <Divider />
-          <div className={classes.list}>
-            <Button className={SwipeableDrawerStyle.drawerButton}>Perfil</Button>
-            <br />
-            <Button className={SwipeableDrawerStyle.drawerButton}>Vehículos</Button>
-            <br />
-            <Button className={SwipeableDrawerStyle.drawerButton}>Info</Button>
-            <br />
-            <Button className={SwipeableDrawerStyle.drawerButton}>Top Sites</Button>
-          </div>
-          <Divider />
-          <div className={classes.info}>
-            <Button size={'small'}>
-              <FontAwesome.FaGithub size={20} style={{ color: 'black' }} />
-            </Button>
-            <Button size={'small'}>
-              <FontAwesome.FaTwitter size={20} style={{ color: 'black' }} />
-            </Button>
-            <Button size={'small'}>
-              <FontAwesome.FaFacebookSquare size={20} style={{ color: 'black' }} />
-            </Button>
-            <Button size={'small'}>
-              <FontAwesome.FaInfoCircle size={20} style={{ color: 'black' }} />
-            </Button>
-          </div>
-        </div>
-      </SwipeableDrawer>
-    </div>;
-  }
+	render() {
+		const { classes } = this.props;
+		return <div>
+			<div className={SwipeableDrawerStyle.topStuff}>
+				<div className={SwipeableDrawerStyle.menuButton}>
+					<Button onClick={this.toggleDrawer}><FontAwesome.FaBars size={30} /></Button>
+				</div>
+				<div className={SwipeableDrawerStyle.topButton}>
+					<button onClick={this.onChange} className={SwipeableDrawerStyle.funcButton}><span>Park</span></button>
+					<button className={SwipeableDrawerStyle.funcButton}><span>Move</span></button>
+					<button className={SwipeableDrawerStyle.funcButton}><span>Where did I park?</span></button>
+					<button className={SwipeableDrawerStyle.funcButton} style={{ fontSize: '14px' }}><span>Where should I park?<FontAwesome.FaStar /></span></button>
+				</div>
+			</div>
+			<SwipeableDrawer open={this.state.open} onClose={this.toggleDrawer} onOpen={this.toggleDrawer}>
+				<div className={classes.drawer} tabIndex={0} role='button' onClick={this.toggleDrawer} onKeyDown={this.toggleDrawer}>
+					<div className={classes.UserIcon}><FontAwesome.FaUser size={40} /></div>
+					<Divider />
+					<div className={classes.list}>
+						<Button className={SwipeableDrawerStyle.drawerButton}>Perfil</Button>
+						<br />
+						<Button className={SwipeableDrawerStyle.drawerButton}>Vehículos</Button>
+						<br />
+						<Button className={SwipeableDrawerStyle.drawerButton}>Info</Button>
+						<br />
+						<Button className={SwipeableDrawerStyle.drawerButton}>Top Sites</Button>
+					</div>
+					<Divider />
+					<div className={classes.info}>
+						<Button size={'small'}>
+							<FontAwesome.FaGithub size={20} style={{ color: 'black' }} />
+						</Button>
+						<Button size={'small'}>
+							<FontAwesome.FaTwitter size={20} style={{ color: 'black' }} />
+						</Button>
+						<Button size={'small'}>
+							<FontAwesome.FaFacebookSquare size={20} style={{ color: 'black' }} />
+						</Button>
+						<Button size={'small'}>
+							<FontAwesome.FaInfoCircle size={20} style={{ color: 'black' }} />
+						</Button>
+					</div>
+				</div>
+			</SwipeableDrawer>
+		</div>;
+	}
 }
 
 export default withStyles(styles)(SwipeableTemporaryDrawer);
